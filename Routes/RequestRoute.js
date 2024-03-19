81c1e286339e9ef710167c559b65e2b4d1590459
musaddiq25 <90511493+musaddiq25@users.noreply.github.com>
2024-03-20 00:39:59 +0530

    bug fixes

--- Routes/RequestRoute.js
+++ Routes/RequestRoute.js
@@ -1,14 +1,17 @@
 import { Router } from 'express';
-import { editEquipment, getAllAndSearchEquipment, getEquipmentByCompanyIdAndSearch, uploadEquipment, deleteEquipment } from '../Controllers/EquipmentController.js';
+import { createRequest, getUserRequests, manageRequestStatus, getUserOwnedRequests, getAllUserRequest } from '../Controllers/RequestController.js';
 
 const router = Router();
 
 
-router.post("/add", uploadEquipment.validator, uploadEquipment.controller);
-router.put('/edit-equipment', editEquipment.validator, editEquipment.controller);
-router.put('/delete-equipment', deleteEquipment.validator, deleteEquipment.controller);
-router.get('/getAllAndSearchEquipment', getAllAndSearchEquipment.controller)
-router.get('/getEquipmentByCompanyIdAndSearch', getEquipmentByCompanyIdAndSearch.validator, getEquipmentByCompanyIdAndSearch.controller)
+router.post("/createRequest", createRequest.validator, createRequest.controller);
+router.get("/getUserRequests", getUserRequests.validator, getUserRequests.controller);
+router.get("/getUserOwnedRequests", getUserOwnedRequests.validator, getUserOwnedRequests.controller);
+router.get("/getAllUserRequest", getAllUserRequest.validator, getAllUserRequest.controller);
+
+// getAllUserRequest
+router.post("/manageRequestStatus", manageRequestStatus.validator, manageRequestStatus.controller);
+
 
 
 
